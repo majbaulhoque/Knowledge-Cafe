fd1b7cf906e1ea5384195ee874a1df67d251814d
majbaulhoque <majba9399@gmail.com>
2023-12-10 14:41:02 +0600

    data loaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { useState } from 'react'
 import './App.css'
+import Blogs from './Components/Blogs/Blogs'
 import Header from './Components/Header/Header'
+import { useEffect } from 'react';
 
 function App() {
+  const [blogs, setBlogs] = useState([]);
 
+  useEffect( () => {
+    fetch('blogs.json')
+    .then(res => res.json())
+    .then(data => setBlogs(data))
+  },[])
   return (
     <>
       <Header></Header>
+      <Blogs></Blogs>
     </>
   )
 }
