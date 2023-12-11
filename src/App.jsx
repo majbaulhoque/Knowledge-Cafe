682ba2e5e1e5b53731260806bf1157ccff28ab68
majbaulhoque <majba9399@gmail.com>
2023-12-11 12:25:18 +0600

    Added Bookmark button and lift up the state

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
+import { useState } from 'react'
 import './App.css'
 import Blogs from './Components/Blogs/Blogs'
 import Bookmarks from './Components/Bookmarks/Bookmarks'
 import Header from './Components/Header/Header'
 
 function App() {
+  const [bookmarks, setBookmarks] = useState([]);
+
+  const handleAddToBookmark = blog =>{
+    console.log('Bookmark adding soon')
+  }
+
   return (
     <div className='max-w-7xl mx-auto'>
       <Header></Header>
       <div className='md: flex'>
-        <Blogs></Blogs>
+        <Blogs handleAddToBookmark={handleAddToBookmark} ></Blogs>
         <Bookmarks></Bookmarks>
       </div>
     </div>
--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,25 +1,27 @@
 import PropTypes from 'prop-types'
+import { IoBookmarksOutline } from "react-icons/io5";
 
-const Blog = ({blog}) => {
+const Blog = ({blog, handleAddToBookmark}) => {
     const {title,cover_img, author, author_img, posted_date, hashtags, reading_time} = blog;
     return (
-        <div>
-            <img className='w-[885px]' src={cover_img} alt={`Cover picture of the title ${title}`} />
+        <div className='mb-20'>
+            <img className='w-full mt-4 mb-6 rounded-xl' src={cover_img} alt={`Cover picture of the title ${title}`} />
             <div className='flex justify-between'>
                 <div className='flex p-2'>
-                    <img className='w-14 h-14' src={author_img}alt="" />
+                    <img className='w-14 h-14 mb-4' src={author_img}alt="" />
                     <div className='ml-6'>
-                        <h3 className='text-lg'>{author}</h3>
+                        <h3 className='text-lg mb-1'>{author}</h3>
                         <p>{posted_date}</p>
                     </div>
                 </div>
                 
-                <div>
+                <div className='mt-2'>
                     <span>{reading_time} min read</span>
+                    <button onClick={() => handleAddToBookmark()} className='ml-2'><IoBookmarksOutline></IoBookmarksOutline></button>
                 </div>
             </div>
             <h3 className='text-4xl'> {title}</h3>
-            <p>
+            <p className='mt-4'>
                 {
                     hashtags.map((hash, idx) => <span key={idx}><a href=''>#{hash}</a></span>)
                 }
