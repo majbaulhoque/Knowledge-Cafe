10f96b2b5f8367d2c0073c8718015e5a01e7c311
majbaulhoque <majba9399@gmail.com>
2023-12-11 11:40:10 +0600

    wevsite centering

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,13 @@ import Header from './Components/Header/Header'
 
 function App() {
   return (
-    <>
+    <div className='max-w-7xl mx-auto'>
       <Header></Header>
       <div className='md: flex'>
         <Blogs></Blogs>
         <Bookmarks></Bookmarks>
       </div>
-    </>
+    </div>
   )
 }
 
