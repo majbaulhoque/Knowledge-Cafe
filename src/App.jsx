ee708fe9e929fb775bb383730b4bb75c007affee
majbaulhoque <majba9399@gmail.com>
2023-12-26 13:41:53 +0600

    set layout and make react responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,18 @@
-import { useState } from 'react'
+
 import './App.css'
 import Blogs from './Components/Blogs/Blogs'
 import Bookmarks from './Components/Bookmarks/Bookmarks'
 import Header from './Components/Header/Header'
 
+
 function App() {
-  const [bookmarks, setBookmarks] = useState([]);
-  const [readingTime, setReadingTime] = useState(0)
-
-  const handleAddToBookmark = blog =>{
-    const newBookMarks = [...bookmarks, blog];
-    setBookmarks(newBookMarks)
-  }
-
-  const handleMarkAsRead = (id, time) =>{
-    setReadingTime(readingTime + time);
-    // ! Remove the blog from bookmark
-    // console.log('remove bookmark', id)
-    const remainingBookmarks = bookmarks.filter(bookmark => bookmark.id !== id);
-    setBookmarks(remainingBookmarks)
-  }
-
   return (
-    <div className='max-w-7xl mx-auto'>
-      <Header></Header>
-      <div className='md: flex'>
-        <Blogs handleAddToBookmark={handleAddToBookmark} handleMarkAsRead={handleMarkAsRead} ></Blogs>
-        <Bookmarks bookmarks={bookmarks} readingTime={readingTime}></Bookmarks>
-      </div>
+    <div>
+        <Header></Header>
+        <div className='md: flex'>
+          <Blogs></Blogs>
+          <Bookmarks></Bookmarks>
+        </div>
     </div>
   )
 }
--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,40 +1,15 @@
 import PropTypes from 'prop-types'
-import { IoBookmarksOutline } from "react-icons/io5";
 
-const Blog = ({blog, handleAddToBookmark,handleMarkAsRead}) => {
-    const {id,title,cover_img, author, author_img, posted_date, hashtags, reading_time} = blog;
+const Blog = ({blog}) => {
     return (
-        <div className='mb-20'>
-            <img className='w-full mt-4 mb-6 rounded-xl' src={cover_img} alt={`Cover picture of the title ${title}`} />
-            <div className='flex justify-between'>
-                <div className='flex p-2'>
-                    <img className='w-14 h-14 mb-4' src={author_img}alt="" />
-                    <div className='ml-6'>
-                        <h3 className='text-lg mb-1'>{author}</h3>
-                        <p>{posted_date}</p>
-                    </div>
-                </div>
-                
-                <div className='mt-2'>
-                    <span>{reading_time} min read</span>
-                    <button onClick={() => handleAddToBookmark(blog)} className='ml-2 '><IoBookmarksOutline></IoBookmarksOutline></button>
-                </div>
-            </div>
-            <h3 className='text-4xl'> {title}</h3>
-            <p className='mt-4'>
-                {
-                    hashtags.map((hash, idx) => <span key={idx}><a href=''>{hash}</a></span>)
-                }
-            </p>
-            <button onClick={() =>handleMarkAsRead(id, reading_time)} className='text-purple-800 mt-4 font-bold underline'>Mark As Read</button>
+        <div>
+            
         </div>
     );
 };
 
 Blog.propTypes = {
-    blog: PropTypes.object.isRequired,
-    handleAddToBookmark: PropTypes.func,
-    handleMarkAsRead: PropTypes.func
+    blog: PropTypes.object.isRequired
 }
 
 export default Blog;
